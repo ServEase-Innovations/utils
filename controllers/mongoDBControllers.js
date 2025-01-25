33f9cc5730b89d8aa34ec6e7b330882390e48ddf
Subir-Droid <subirdeyabhi@gmail.com>
2025-01-25 19:44:34 +0530

    remove if else

--- controllers/mongoDBControllers.js
+++ controllers/mongoDBControllers.js
@@ -276,17 +276,18 @@ async function addRecord(newRecord) {
 
      console.log("result ",result)
  
-     if (result.modifiedCount > 0) {
-       console.log("Record updated successfully!");
-       return res.status(200).json({ message: "Record updated successfully!" });
-     } else {
-       console.log("No document was updated.");
-       return res.status(404).json({ message: "No record found to update." });
-     }
+    //  if (result.modifiedCount > 0) {
+    //    console.log("Record updated successfully!");
+    //    return res.status(200).json({ message: "Record updated successfully!" });
+    //  } else {
+    //    console.log("No document was updated.");
+    //    return res.status(404).json({ message: "No record found to update." });
+    //  }
    } catch (error) {
      console.error("Error occurred while updating record:", error);
      return res.status(500).json({ message: "Internal server error." });
-   } finally {
+   } 
+   finally {
      await client.close();
    }
  }
